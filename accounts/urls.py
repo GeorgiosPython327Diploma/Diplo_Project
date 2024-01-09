2d5f25b940859ffc4c419200fcb9e853f025bb46
Georgio <georgetrueman64@gmail.com>
2024-01-09 15:15:59 +0300

    update new

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegistrationView, LoginView
+from .views import RegistrationView, LoginView, user_profile
 
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('user_profile/', user_profile, name='user_profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.views.generic.edit import FormView
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
+from django.shortcuts import render
 
 class RegistrationView(FormView):
     template_name = 'registration/register.html'
@@ -15,7 +17,7 @@ class RegistrationView(FormView):
 class LoginView(FormView):
     template_name = 'registration/login.html'
     form_class = AuthenticationForm
-    success_url = 'core/base.html'
+    success_url = reverse_lazy('user_profile')
 
     def form_valid(self, form):
         username = form.cleaned_data['username']
@@ -23,3 +25,6 @@ class LoginView(FormView):
         user = form.get_user()
         login(self.request, user)
         return super().form_valid(form)
+@login_required
+def user_profile(request):
+    return render(request, 'accounts/user_profile.html', {'user': request.user})
