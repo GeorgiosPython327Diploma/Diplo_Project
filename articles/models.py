5b523e5259c241c9b242b9700b8ed52a5bb38801
Georgio <georgetrueman64@gmail.com>
2024-01-18 18:39:13 +0300

    update

---
+++ articles/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.conf import settings
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on {self.article.title}"
+
+class Bookmark(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} bookmarked {self.article.title}"
---
+++ articles/views.py
@@ -0,0 +1,101 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Article, Comment, Bookmark
+from .forms import ArticleForm, CommentForm
+
+BASE_TEMPLATE = 'core/base.html'
+def base_with_articles(request):
+    articles = Article.objects.order_by('-created_at')
+    article_form = ArticleForm()
+    comment_form = CommentForm()
+    user_has_bookmarks = False
+    if request.user.is_authenticated:
+        user_has_bookmarks = Bookmark.objects.filter(user=request.user, article__in=articles).exists()
+
+    return render(request, 'BASE_TEMPLATE', {'articles': articles, 'article_form': article_form, 'comment_form': comment_form, 'user_has_bookmarks': user_has_bookmarks})
+
+def article_detail(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+
+    user_has_bookmark = False
+    if request.user.is_authenticated:
+        user_has_bookmark = article.bookmark_set.filter(user=request.user).exists()
+
+    return render(request, 'BASE_TEMPLATE', {'article': article, 'user_has_bookmark': user_has_bookmark})
+
+@login_required()
+def add_article(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user
+            article.save()
+
+            return redirect('base_with_articles')
+    else:
+        form = ArticleForm()
+
+    return render(request, 'articles/add_article.html', {'form': form})
+
+@login_required()
+def add_comment(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.article = article
+            comment.author = request.user
+            comment.save()
+
+            return redirect('base_with_articles', pk=pk)
+    else:
+        form = CommentForm()
+
+    return render(request, 'BASE_TEMPLATE', {'form': form})
+
+@login_required()
+def like_article(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    article.likes += 1
+    article.save()
+
+    return HttpResponseRedirect(reverse('base_with_articles', args=(article.id,)))
+
+@login_required()
+def dislike_article(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    article.dislikes += 1
+    article.save()
+
+    return HttpResponseRedirect(reverse('base_with_articles', args=(article.id,)))
+
+@login_required()
+def add_bookmark(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    user = request.user
+
+    if not Bookmark.objects.filter(user=user, article=article).exists():
+        bookmark = Bookmark(user=user, article=article)
+        bookmark.save()
+
+    return redirect('base_with_articles')
+
+@login_required()
+def remove_bookmark(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    user = request.user
+
+    bookmark = Bookmark.objects.filter(user=user, article=article).first()
+    if bookmark:
+        bookmark.delete()
+
+    return redirect('base_with_articles')
+
+def user_bookmarks(request):
+    bookmarks = Bookmark.objects.filter(user=request.user)
+
+    return render(request, 'articles/user_bookmarks.html', {'bookmarks': bookmarks})
