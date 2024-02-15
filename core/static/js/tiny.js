4699beb63f25f87e7e155aaa8144eb1d1c7e90f9
Georgio <georgetrueman64@gmail.com>
2024-02-15 06:38:44 +0300

    update

--- core/static/js/tiny.js
+++ core/static/js/tiny.js
@@ -3,15 +3,15 @@ document.addEventListener("DOMContentLoaded", function() {
     selector: 'textarea.content_comment',
     height: 200,
     width: 800,
-    plugins: 'autoresize autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table contextmenu paste code',
-    toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+    plugins: 'autoresize  autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table contextmenu paste code emoticons',
+    toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | emoticons',
   });
 
   tinymce.init({
     selector: 'textarea.content_edit',
     height: 500,
     menubar: true,
-    plugins: 'autoresize autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table contextmenu paste code',
-    toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+    plugins: 'autoresize autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table contextmenu paste code emoticons',
+    toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | emoticons',
   });
 });
