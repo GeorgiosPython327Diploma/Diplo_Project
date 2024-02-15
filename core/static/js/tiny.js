16eac4dcc1197aea5405f83a905eab185d4ea33d
Georgio <georgetrueman64@gmail.com>
2024-02-15 18:14:55 +0300

    update

--- core/static/js/tiny.js
+++ core/static/js/tiny.js
@@ -4,7 +4,8 @@ document.addEventListener("DOMContentLoaded", function() {
     height: 200,
     width: 800,
     plugins: 'autoresize  autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table contextmenu paste code emoticons',
-    toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | emoticons',
+    toolbar: 'undo redo | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | emoticons',
+    invalid_elements: 'h1,h2,h3,h4,h5,h6',
   });
 
   tinymce.init({
@@ -12,6 +13,7 @@ document.addEventListener("DOMContentLoaded", function() {
     height: 500,
     menubar: true,
     plugins: 'autoresize autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table contextmenu paste code emoticons',
-    toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | emoticons',
+    toolbar: 'undo redo | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | emoticons',
+    invalid_elements: 'h1,h2,h3,h4,h5,h6',
   });
 });
