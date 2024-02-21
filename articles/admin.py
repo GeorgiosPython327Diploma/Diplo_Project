9b0a7982735f2a80c671c26ddd432965deafac5f
Georgio <georgetrueman64@gmail.com>
2024-02-22 01:54:09 +0300

    new

--- articles/admin.py
+++ articles/admin.py
@@ -19,7 +19,7 @@ class ArticleAdminForm(forms.ModelForm):
 
 class ArticleAdmin(admin.ModelAdmin):
     form = ArticleAdminForm
-    list_display = ('title', 'author', 'created_at', 'get_likes_count', 'get_dislikes_count')
+    list_display = ('title', 'author', 'created_at', 'get_likes_count', 'get_dislikes_count', 'category')
 
     def get_likes_count(self, obj):
         return obj.likes.count()
@@ -27,14 +27,13 @@ class ArticleAdmin(admin.ModelAdmin):
     def get_dislikes_count(self, obj):
         return obj.dislikes.count()
 
-    def get_like_dislike_count(self, obj):
-        likes = obj.likes.count()
-        dislikes = obj.dislikes.count()
-        return f'Likes: {likes}, Dislikes: {dislikes}'
+    def display_categories(self, obj):
+        return ", ".join([category.name for category in obj.category.all()])
+
+    display_categories.short_description = 'Категории'
 
     get_likes_count.short_description = 'Likes'
     get_dislikes_count.short_description = 'Dislikes'
-    get_like_dislike_count.short_description = 'Like/Dislike Counts'
 
 
 class BookmarkAdmin(admin.ModelAdmin):
