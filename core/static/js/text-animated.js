65dd84859648a630d6006d587c652fcc1ea2dfcb
Georgio <georgetrueman64@gmail.com>
2024-03-03 23:46:39 +0300

    new

--- core/static/js/text-animated.js
+++ core/static/js/text-animated.js
@@ -2,18 +2,26 @@ gsap.from("#welcomeText", {
   duration: 1,
   text: { value: "Welcome", newClass: "highlight" },
   ease: "power1.inOut",
+  stagger: {
+    each: 0.2,
+    from: "end"
+  },
   onComplete: function() {
     gsap.to(".rotate-char", {
-      duration: 0.6,
-      rotateY: -180,
-      transformOrigin: "50% 50",
+      duration: 1,
+      rotateY: 360,
+      transformOrigin: "50% 50%",
       ease: "power1.inOut",
       onComplete: function() {
         gsap.to(".rotate-char", {
           duration: 0.6,
           rotateY: 0,
           transformOrigin: "50% 50%",
-          ease: "power1.inOut"
+          ease: "power1.inOut",
+          stagger: {
+            each: 0.1,
+            from: "start"
+          },
         });
       }
     });
