35c0eaf9c545ecc1b7591f22977ee34d5f61d865
Georgio <georgetrueman64@gmail.com>
2024-03-07 07:16:46 +0300

    new

--- core/static/js/transformation.js
+++ core/static/js/transformation.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    $(".login-menu, .register-menu, .change-password-menu, .add-article-container, .edit-container-article, #bio-text-container, #bio-container").css({
+    $(".login-menu, .register-menu, .change-password-menu, .add-article-container, .edit-container-article, #bio-text-container, #bio-container, .static-bio").css({
         "transform": "translateY(100%) translateX(-100%)",
         "transition": "transform ease-in-out 0.8s",
     });
@@ -9,7 +9,7 @@ window.onload = function() {
             "transform": "translateY(0)",
         });
 
-        $("#bio-text-container, #bio-container").css({
+        $("#bio-text-container, #bio-container, .static-bio").css({
             "transform": "translateY(0) translateX(0)",
         });
     }, 100);
