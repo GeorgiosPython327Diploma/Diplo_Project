6ce4b58dfd767abe96579ac3cbd2e38347585068
Georgio <georgetrueman64@gmail.com>
2024-03-09 15:19:41 +0300

    new

--- core/static/js/edit_profile_animation.js
+++ core/static/js/edit_profile_animation.js
@@ -2,12 +2,16 @@ document.addEventListener("DOMContentLoaded", function () {
     const formContainer = document.querySelector('.form-container');
     const bioAvatar = document.querySelector('.form-bio-avatar');
     const additionalInfo = document.querySelector('.edit-form-country-city-occupation-gender-age');
+    const saveButton = document.querySelector('.but-edit-profile');
 
     gsap.set(formContainer, { transform: 'translateY(100%)' });
     gsap.set(bioAvatar, { transform: 'translateY(-40%)' });
     gsap.set(additionalInfo, { transform: 'translateY(-25%)' });
+    gsap.set(saveButton, { opacity: 0 });
 
-    gsap.to(formContainer, { transform: 'translateY(0)', duration: 1, ease: 'ease-in-out' });
-    gsap.to(bioAvatar, { transform: 'translateY(0)', duration: 0.7, ease: 'ease-in-out', delay: 0.7 });
-    gsap.to(additionalInfo, { transform: 'translateY(0)', duration: 0.7, ease: 'ease-in-out', delay: 0.6 });
+    gsap.to(formContainer, { transform: 'translateY(0)', duration: 1, ease: 'power1.inOut' });
+    gsap.to(bioAvatar, { transform: 'translateY(0)', duration: 0.7, ease: 'power1.inOut', delay: 0.7 });
+    gsap.to(additionalInfo, { transform: 'translateY(0)', duration: 0.7, ease: 'power1.inOut', delay: 0.6 });
+
+    gsap.to(saveButton, { opacity: 1, duration: 0.6, ease: 'power3.inOut', delay: 1.1 });
 });
