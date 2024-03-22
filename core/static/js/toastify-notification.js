6df3db48ad04b68ca73011e071d0ed0cc1b4d012
Georgio <georgetrueman64@gmail.com>
2024-03-22 06:57:48 +0300

    final update

--- core/static/js/toastify-notification.js
+++ core/static/js/toastify-notification.js
@@ -1,28 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
     var notification = document.getElementById('notification');
 
-    // Показываем уведомление
     function showNotification(sender) {
         notification.innerText = `У вас новые сообщения от ${sender}!`;
         notification.style.display = 'block';
     }
 
-    // Скрываем уведомление
     function hideNotification() {
         notification.style.display = 'none';
     }
 
-    // Функция для обновления счетчика непрочитанных сообщений и отображения уведомления
     function updateUnreadCountAndNotification() {
-        fetch('/accounts/unread_message_count/') // Используйте абсолютный путь к вашему представлению
+        fetch('/accounts/unread_message_count/')
             .then(response => response.json())
             .then(data => {
                 var unreadCount = data.unread_count;
-                var sender = data.sender || "Анонимный отправитель"; // Если sender не определен, используйте альтернативное значение
+                var sender = data.sender || "Анонимный отправитель";
                 if (unreadCount > 0) {
-                    showNotification(sender); // Показываем уведомление с именем отправителя, если есть непрочитанные сообщения
+                    showNotification(sender);
                 } else {
-                    hideNotification(); // Скрываем уведомление, если нет непрочитанных сообщений
+                    hideNotification();
                 }
             })
             .catch(error => {
@@ -30,9 +27,7 @@ document.addEventListener("DOMContentLoaded", function() {
             });
     }
 
-    // Первоначальное обновление счетчика непрочитанных сообщений и отображение уведомления
     updateUnreadCountAndNotification();
 
-    // Повторяем обновление счетчика и отображение уведомления каждые 5 секунд
     setInterval(updateUnreadCountAndNotification, 5000);
 });
