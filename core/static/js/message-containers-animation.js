518429c9d9aedce47da0df6e0faac22fb563c834
Georgio <georgetrueman64@gmail.com>
2024-04-22 15:01:17 +0300

    update

--- core/static/js/message-containers-animation.js
+++ core/static/js/message-containers-animation.js
@@ -2,10 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
   const inboxMessage = document.querySelector('.inbox-message');
   const composeMessage = document.querySelector('.compose-message');
   const backProfileLink = document.querySelector('.back-profile');
+  const delBut = document.querySelector('.del-messages-but');
 
   gsap.set(backProfileLink, { opacity: 0, y: '50%' });
+  gsap.set(delBut, { opacity: 0, y: '50%' });
 
   gsap.to(backProfileLink, { opacity: 1, y: '0%', duration: 1, ease: 'power1.inOut', delay: 0.5 });
+  gsap.to(delBut, { opacity: 1, y: '0%', duration: 1, ease: 'bounce.inOut', delay: 1 });
 
   gsap.set(inboxMessage, { opacity: 0, y: '100%' });
   gsap.set(composeMessage, { opacity: 0, y: '100%' });
