748e3f104390cc920f75133e18559a9265cc36e5
Georgio <georgetrueman64@gmail.com>
2024-04-25 02:31:02 +0300

    update

--- core/static/js/message-open-window.js
+++ core/static/js/message-open-window.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
   const modal = document.getElementById('modalOverlay');
   const modalContent = document.getElementById('modalContent');
   const closeModal = document.getElementById('closeModalBtn');
+  const inboxMessage = document.querySelector('.inbox-message');
+  const delMessagesBtn = document.querySelector('.del-messages-but');
+  const backProfileLink = document.querySelector('.back-profile');
 
   viewMessageBtns.forEach(btn => {
     btn.addEventListener('click', function(event) {
@@ -10,10 +13,28 @@ document.addEventListener('DOMContentLoaded', function() {
       const messageContent = this.getAttribute('data-content');
       modalContent.innerHTML = messageContent;
       modal.style.display = 'block';
+      hideElements();
     });
   });
 
   modal.addEventListener('click', function() {
     modal.style.display = 'none';
+    showElements();
   });
+
+  modal.addEventListener('click', function(event) {
+    event.stopPropagation();
+  });
+
+  function hideElements() {
+    inboxMessage.style.display = 'none';
+    delMessagesBtn.style.display = 'none';
+    backProfileLink.style.display = 'none';
+  }
+
+  function showElements() {
+    inboxMessage.style.display = 'block';
+    delMessagesBtn.style.display = 'block';
+    backProfileLink.style.display = 'block';
+  }
 });
