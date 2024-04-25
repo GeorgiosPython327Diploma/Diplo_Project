a735c961374b9de59f3011839b6ede1aefd9bc65
Georgio <georgetrueman64@gmail.com>
2024-04-25 04:43:03 +0300

    new

--- core/static/js/message-open-window.js
+++ core/static/js/message-open-window.js
@@ -7,34 +7,53 @@ document.addEventListener('DOMContentLoaded', function() {
   const delMessagesBtn = document.querySelector('.del-messages-but');
   const backProfileLink = document.querySelector('.back-profile');
 
+  function showModal() {
+    modal.style.display = 'block';
+
+    modal.classList.remove('modal-enter', 'modal-enter-active');
+
+    modal.classList.add('modal-enter');
+
+    setTimeout(() => {
+      modal.classList.add('modal-enter-active');
+    }, 300);
+  }
+
+  function hideModal() {
+    modal.classList.remove('modal-enter-active');
+
+    setTimeout(() => {
+      modal.style.display = 'none';
+    }, 500);
+  }
+
+  function hideElements() {
+    gsap.to([inboxMessage, delMessagesBtn, backProfileLink], { duration: 0.5, opacity: 0, display: 'none' });
+  }
+
+  function showElements() {
+    gsap.to([inboxMessage, delMessagesBtn, backProfileLink], { duration: 0.5, opacity: 1, display: 'block' });
+  }
+
   viewMessageBtns.forEach(btn => {
     btn.addEventListener('click', function(event) {
       event.preventDefault();
       const messageContent = this.getAttribute('data-content');
       modalContent.innerHTML = messageContent;
-      modal.style.display = 'block';
+      showModal();
       hideElements();
     });
   });
 
-  modal.addEventListener('click', function() {
-    modal.style.display = 'none';
+  closeModal.addEventListener('click', function() {
+    hideModal();
     showElements();
   });
 
   modal.addEventListener('click', function(event) {
-    event.stopPropagation();
+    if (event.target === modal) {
+      hideModal();
+      showElements();
+    }
   });
-
-  function hideElements() {
-    inboxMessage.style.display = 'none';
-    delMessagesBtn.style.display = 'none';
-    backProfileLink.style.display = 'none';
-  }
-
-  function showElements() {
-    inboxMessage.style.display = 'block';
-    delMessagesBtn.style.display = 'block';
-    backProfileLink.style.display = 'block';
-  }
 });
