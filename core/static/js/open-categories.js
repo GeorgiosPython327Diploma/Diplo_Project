8b760f309471d3324b093435685052a0facf205b
Georgio <georgetrueman64@gmail.com>
2024-05-05 07:01:19 +0300

    update

--- core/static/js/open-categories.js
+++ core/static/js/open-categories.js
@@ -1,39 +1,56 @@
 document.addEventListener("DOMContentLoaded", function () {
     const categoriesList = document.querySelector('.categories-article ul');
     const categoriesItems = document.querySelectorAll('.categories-article li');
-    const maxTranslateX = 0.7;
+    const shape = document.querySelector('.shape');
+    let isOpen = false;
 
-    let lastScrollY = window.scrollY;
-
-    window.addEventListener("wheel", function(event) {
-        const scrollPercentage = (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100;
-        const direction = event.deltaY > 0 ? 'forward' : 'backward';
+    const openMenu = () => {
+        gsap.to('.categories-article', {
+            x: 100,
+            duration: 0.5,
+            ease: 'power1.inOut',
+        });
 
         categoriesItems.forEach((item, index) => {
-            let parallaxValue = (scrollPercentage * (index + 1)) / 100;
-
-            parallaxValue = Math.min(parallaxValue, maxTranslateX);
-
             gsap.to(item, {
-                x: parallaxValue * 100,
-                opacity: direction === 'backward' ? 0 : 1,
-                duration: 0.3,
+                x: 0,
+                opacity: 1,
+                duration: 0.5,
+                ease: 'power1.inOut',
+                delay: index * 0.1
+            });
+        });
+    };
+
+    const closeMenu = () => {
+        gsap.to('.categories-article', {
+            x: -100,
+            duration: 0.5,
+            ease: 'power1.inOut',
+        });
+
+        categoriesItems.forEach((item, index) => {
+            gsap.to(item, {
+                x: 0,
+                opacity: 0,
+                duration: 0.5,
                 ease: 'power1.inOut',
             });
         });
+    };
 
-        lastScrollY = window.scrollY;
-
-        if (direction === 'backward') {
-            gsap.to(categoriesItems, {
-                x: -100,
-                opacity: 0,
-                scale: 1,
-                duration: 0.3,
-                ease: 'power1.inOut',
-            });
+    const toggleMenu = () => {
+        if (!isOpen) {
+            openMenu();
+        } else {
+            closeMenu();
         }
-    });
+        isOpen = !isOpen;
+    };
+
+    openMenu();
+
+    shape.addEventListener("click", toggleMenu);
 
     categoriesItems.forEach(item => {
         item.addEventListener("mouseenter", function () {
